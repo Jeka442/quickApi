793835c9d717226fc2f3cc2ff12d2bfa55fcb420
Eivgeniy Gudilov <eivgeniygu@matrix.co.il>
2024-05-08 19:17:43 +0300

    done

--- renderer.js
+++ renderer.js
@@ -1,10 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const jsonInput = document.getElementById("json-input");
-  document.getElementById("json-input").value =
-    window.localStorage.getItem("json-input") ?? "";
-  setJson();
+const basePath = "http://localhost:3020/";
 
-  jsonInput.addEventListener("keydown", () => {
+const jsonInput = document.getElementById("json-input");
+const sync = document.getElementById("sync");
+const apiSelect = document.getElementById("api-select");
+const editNameBtn = document.getElementById("edit-name");
+const addNameBtn = document.getElementById("add-name");
+const submitDialog = document.querySelector("#submit-name");
+const submitNameBtn = document.querySelector("#submit-name > button");
+const submitNameInput = document.querySelector("#submit-name > input");
+
+function setToTextarea(obj) {
+  jsonInput.value = JSON.stringify(obj, null, 3);
+}
+
+function setSync(flag) {
+  sync.setAttribute("data-for-sync", `${flag}`);
+  apiSelect.disabled = !flag;
+  addNameBtn.disabled = !flag;
+  editNameBtn.disabled = !flag;
+}
+function setLoading(flag) {
+  sync.setAttribute("data-for-loading", `${flag}`);
+}
+
+async function onLoad() {
+  const endpoints = await (await fetch(basePath)).json();
+  const options = endpoints.map((endpoint) => {
+    return `<option value=${endpoint}>${endpoint}</option>`;
+  });
+  apiSelect.innerHTML = options;
+  setSync(true);
+  setLoading(true);
+  setToTextarea((await getByName(endpoints[0])) ?? {});
+  setLoading(false);
+}
+onLoad();
+
+async function getByName(name) {
+  const data = await (await fetch(basePath + name)).json();
+  return data;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  jsonInput.addEventListener("keydown", function (event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      const start = this.selectionStart;
+      const end = this.selectionEnd;
+      const space = "   ";
+      this.value =
+        this.value.substring(0, start) + space + this.value.substring(end);
+      this.selectionStart = this.selectionEnd = start + space.length;
+    }
+    setSync(false);
     document.getElementById("error").style.display = "none";
   });
 
@@ -23,7 +71,6 @@ document.addEventListener("DOMContentLoaded", () => {
           const reader = new FileReader();
           reader.onload = function (e) {
             jsonInput.value = e.target.result;
-            beautify();
           };
           reader.readAsText(file);
         }
@@ -31,7 +78,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-
 
 function closeWindow() {
   window.electronAPI.closeWindow(); // Calls the function exposed via preload.js
@@ -49,24 +95,130 @@ function beautify() {
   }
 }
 
-function setJson() {
+async function setJson() {
   try {
     const value = document.getElementById("json-input").value;
+    const name = apiSelect.value;
     if (value && value.length > 0) {
       const json = JSON.parse(value);
-      window.localStorage.setItem("json-input", value);
-      fetch("http://localhost:3020/upload-json", {
+      setLoading(true);
+
+      const result = await fetch(basePath + "setData", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(json),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((err) => console.error("Error posting JSON", err));
+        body: JSON.stringify({
+          name: name,
+          obj: json,
+        }),
+      });
+      if (result.status === 200) {
+        setSync(true);
+      }
+      setLoading(false);
     }
   } catch {
+    setLoading(false);
     document.getElementById("error").style.display = "block";
   }
+}
+
+async function changeApi(e) {
+  setToTextarea(await getByName(e.value));
+}
+
+function addName() {
+  submitDialog.style.height = "100%";
+  submitNameBtn.setAttribute("data-for", "add");
+}
+
+function editName() {
+  submitDialog.style.height = "100%";
+  submitNameBtn.setAttribute("data-for", "edit");
+}
+
+async function submitNameHandler(doc) {
+  const type = doc.getAttribute("data-for");
+  const newName = submitNameInput.value;
+
+  if (type && newName && newName.length > 0) {
+    setLoading(true);
+    if (type === "add") {
+      const result = await fetch(basePath + "name", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: newName,
+        }),
+      });
+      switch (result.status) {
+        case 201: {
+          apiSelect.innerHTML =
+            apiSelect.innerHTML +
+            `<option value="${newName}">${newName}</option>`;
+          apiSelect.value = newName;
+          jsonInput.value = "{}";
+          break;
+        }
+        case 409: {
+          apiSelect.value = newName;
+          break;
+        }
+        default: {
+          alert("something went wrong");
+        }
+      }
+    } else {
+      const result = await fetch(basePath + "name", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: apiSelect.value,
+          newName: newName,
+        }),
+      });
+      if (result.status === 200) {
+        const optionDoc = document.querySelector(
+          `option[value="${apiSelect.value}"]`
+        );
+        optionDoc.value = newName;
+        optionDoc.innerHTML = newName;
+        apiSelect.value = newName;
+      } else {
+        submitNameInput.value = "Existed name!";
+        setLoading(false);
+        return;
+      }
+    }
+    submitNameInput.value = "";
+    submitDialog.style.height = "0";
+    setLoading(false);
+  }
+}
+
+async function deleteName() {
+  const optionsLength = document.querySelectorAll("option").length;
+  if (optionsLength === 1) {
+    jsonInput.value = "{}";
+    await setJson();
+
+    setLoading(false);
+    return;
+  }
+  setLoading(true);
+  const result = await fetch(basePath + apiSelect.value, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (result.status === 200) {
+    window.location.reload();
+  }
+  setLoading(false);
 }
--- server.js
+++ server.js
@@ -1,24 +1,102 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
+const fs = require("fs");
 
 app.use(cors({ allowedHeaders: "*" }));
 app.use(express.json());
 
 let storedJson = {}; // To store the JSON received from the app
 
-// Endpoint to receive JSON from the Electron app
-app.post("/upload-json", (req, res) => {
-  storedJson = req.body;
-  res.send({ status: "JSON stored successfully" });
+const dbPath = path.join(__dirname, "db.json");
+async function getDb() {
+  try {
+    const data = JSON.parse(fs.readFileSync(dbPath, "utf8"));
+    return data;
+  } catch {
+    return [];
+  }
+}
+
+// all endpoints
+app.get("/", async (req, res) => {
+  const db = await getDb();
+  res.send(db.map((x) => x.name));
 });
 
-// Endpoint to serve the stored JSON
-app.get("/", (req, res) => {
-  res.json(storedJson);
+app.get("/:id", async (req, res) => {
+  const db = await getDb();
+  const result = db.find((item) => item.name === req.params.id);
+  if (result) {
+    res.json(result.obj);
+    return;
+  }
+  res.status(404).send("not found");
+});
+
+// sets a new endpoint
+app.post("/name", async (req, res) => {
+  const { name } = req.body;
+  const db = await getDb();
+  if (db.find((x) => x.name === name)) {
+    res.sendStatus(409);
+    return;
+  }
+  db.push({ name, obj: {} });
+  fs.writeFileSync(dbPath, JSON.stringify(db, null, 3));
+  res.sendStatus(201);
+});
+
+app.patch("/name", async (req, res) => {
+  const { name, newName } = req.body;
+  const db = await getDb();
+  if (db.find((x) => x.name === name)) {
+    if (db.find((x) => x.name === newName)) {
+      return res.sendStatus(409);
+    }
+
+    let newDb = db.map((item) => {
+      if (item.name === name) {
+        item.name = newName;
+      }
+      return item;
+    });
+    fs.writeFileSync(dbPath, JSON.stringify(newDb, null, 3));
+    res.status(200).send();
+    return;
+  }
+  res.status(404).send("not found");
+});
+
+// add data to an object
+app.post("/setData", async (req, res) => {
+  let db = await getDb();
+  if (!db.find((x) => x.name === req.body.name))
+    return res.status(404).send("not found");
+  db = db.map((item) => {
+    if (item.name === req.body.name) {
+      item.obj = req.body.obj;
+    }
+    return item;
+  });
+  fs.writeFileSync(dbPath, JSON.stringify(db, null, 3));
+  res.status(200).send("ok");
 });
 
 const PORT = 3020;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.delete("/", async (req, res) => {
+  fs.writeFileSync(dbPath, JSON.stringify([], null, 3));
+  res.status(200).send("done");
+});
+
+app.delete("/:name", async (req, res) => {
+  let db = await getDb();
+  db = db.filter((item) => item.name != req.params.name);
+  fs.writeFileSync(dbPath, JSON.stringify(db, null, 3));
+  res.status(200).send("deleted");
 });
